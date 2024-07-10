a0f646cee4c5196ed8c8c387c1bdab7228df99da
NIMMOOO <kumarvtiwari1973@gmail.com>
2024-07-10 20:43:35 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './Component/Assets/Navbar/Navbar';
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css' 
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+const Navbar =()=>{
+    return(
+        <div className='navbar'>
+
+        </div>
+
+    )
+}
+export default Navbar
